44b910fa457dc0f03587b5328ff9cf334f3ba243
Andrei <ftp.andreimiu@gmail.com>
2020-06-22 19:47:53 +0200

    Documentacion doc

--- doc.java
+++ doc.java
@@ -13,5 +13,14 @@ public interface doc
    // Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file.
    public abstract String obtenerLinea(int x, String file);
    
+   /**
+    * @resumen Devuelve la posicion geografica (Latitud y altitud) dandole el nombre de una calle.
+    * @entrada String calle. Una calle.
+    * @salida int latitud int altitud. La posicion geografica
+    * @precondicion No se pueden insertar caracteres raros (tipo $~@€¬). Que la direccion sea valida.
+    * @postcondicion Devuelve la posicion geografica si todo esta bien.
+    */
+   // Devuelve la posicion geografica (latitud y altitud) dada una calle.
+   public abstract int posicionGeografica(String calle);
    
 }
